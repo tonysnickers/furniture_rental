1feaa87d0520712c7cf274a7bfa6dc104e6efc84
mendy <antoine.mendy@hotmail.fr>
2023-12-15 18:11:49 +0100

    feature(back):booking model added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-const Furniture = require('./furniture.model')
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, unique: true, required: false},
     email: {type: String, unique: true, lowercase: true, trim: true, required: true},
     password: {type: String, minlength: 6, required: true},
-    avatar: { type: String,},
+    avatar: { type: String},
     role: {
         type: String,
         enum: ['user', 'admin'],
