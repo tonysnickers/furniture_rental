e212d81f3ad745f9fdadb35eb85132d9ac6bba42
mendy <antoine.mendy@hotmail.fr>
2023-12-17 16:33:15 +0100

    feature(back): booking routes added

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const connectDB = require('../backend/config/db');
 const user_routes = require('../backend/routes/user.routes');
 const funiture_routes = require('../backend/routes/furniture.routes');
+const booking_routes = require('./routes/booking.routes')
 require('dotenv').config()
 const app = express()
 const port = 4000;
 const cors = require('cors')
 const userModel = require('./models/user.model');
 const { verifyToken } = require('./middleware/auth');
+
 
 
 // connecttion mongo DB
@@ -21,6 +23,7 @@ app.use(cors())
 // routes
 app.use('/user', user_routes)
 app.use('/furniture', funiture_routes)
+app.use('/booking', booking_routes)
 
 app.listen(port, () => {
     console.log("le server est lancé sur le port: " + port);
