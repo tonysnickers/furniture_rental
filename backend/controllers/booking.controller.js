7db5235ff1fe9ecd834030b8825b3626e3ca8d1d
mendy <antoine.mendy@hotmail.fr>
2023-12-29 01:11:16 +0100

    feature(back): edit booking added

--- backend/controllers/booking.controller.js
+++ backend/controllers/booking.controller.js
@@ -5,7 +5,6 @@ const userModel = require("../models/user.model");
 module.exports.createBooking = async (req, res) => {
     const { start_date, end_date, productId} = req.body;
     const userId = await userModel.findById(req.user.user_id)
-    const bb = await Booking.find()
     try {
         if (start_date === Booking.startDate) {
             return "veuillez changer la date de départ"
@@ -21,20 +20,13 @@ module.exports.createBooking = async (req, res) => {
                 ownerId: userId,
                 productId: productId
             })
-            res.json({booked: true, message: "votre booking a bien été enregistré", booking})
+            res.json({booked: true, message: "votre réservation a bien été enregistré", booking})
         }
     } catch (error) {
-        res.status(500).json({booked: false, message: "erreur lors du booking"})
+        res.status(500).json({booked: false, message: "erreur lors de la réservation"})
     }
 
-    function calculateTotalPrice(start_date, end_date, total_price) {
-        const startDate = new Date(start_date);
-        const endDate = new Date(end_date);
-        const timeDifference = Math.abs(endDate.getTime() - startDate.getTime());
-        const numberOfDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
-        const pricePerDay = total_price * numberOfDays;
-        return pricePerDay;
-    }
+   
 }
 
 module.exports.getAllBookings = async (req, res) => {
@@ -45,10 +37,10 @@ module.exports.getAllBookings = async (req, res) => {
             const bookings = await Booking.find()
             res.status(200).json(bookings)
         } else {
-            res.json({message: "vous n'avez acces aux booking"})
+            res.json({message: "vous n'avez acces aux réservations"})
         }
     } catch (error) {
-        res.status(400).json({message: "Impossible d'accèder aux booking"})
+        res.status(400).json({message: "Impossible d'accèder aux réservations"})
     }
 }
 
@@ -58,7 +50,7 @@ module.exports.getBooking = async (req, res) => {
         const userId = req.user.user_id
         const bookings = await Booking.find({ _id: bookingId, ownerId: userId })
         if (!bookings) {
-            res.json({message: "Vous n'avez pas de booking"})
+            res.json({message: "Vous n'avez pas de réservation"})
         }
         res.status(200).json(bookings)
     } catch (error) {
@@ -72,7 +64,7 @@ module.exports.getUserBookings = async (req, res) => {
         console.log(userId);
         const bookings = await Booking.find({ ownerId: userId })
         if (!bookings) {
-            res.json({message: "Vous n'avez pas de booking"})
+            res.json({message: "Vous n'avez pas de réservation"})
         }
         res.status(200).json(bookings)
     } catch (error) {
@@ -80,3 +72,48 @@ module.exports.getUserBookings = async (req, res) => {
     }
 }
 
+module.exports.editBooking = async (req, res) => {
+    const {start_date, end_date, total_price} = req.body
+    const userId = req.user.user_id
+    const bookingId = req.params.id
+    let updatedFields = {}
+    try {
+        const booking = await Booking.find({_id: bookingId, ownerId: userId})
+        if (!booking) res.json({message: "Réservation non trouvé"})
+
+        const product = await Furniture.findById(booking[0].productId[0])
+        if (booking || (user.role === "admin" && booking)) {
+            if (start_date) {
+                updatedFields.start_date = start_date
+                if (!end_date) updatedFields.end_date = booking[0].end_date
+            }
+
+            if (end_date) {
+                updatedFields.end_date = end_date 
+                if (!start_date) updatedFields.start_date = booking[0].start_date
+            } 
+
+            // if ( !start_date && !end_date && !total_price) {
+            //     // res.json({message: "Veuillez renseigner les champs"})
+            // }
+
+            if (!total_price) {
+                updatedFields.total_price = calculateTotalPrice(updatedFields.start_date, updatedFields.end_date, product.price_per_day)
+            }
+
+            const newBooking = await Booking.findByIdAndUpdate(bookingId, updatedFields, { new: true })
+            res.status(201).json({success: true, message: 'Votre réservation a été mise à jour', newBooking})
+        } 
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Une erreur est survenue lors de la modification votre réservation' });
+    }
+}
+
+function calculateTotalPrice(start_date, end_date, total_price) {
+    const startDate = new Date(start_date);
+    const endDate = new Date(end_date);
+    const timeDifference = Math.abs(endDate.getTime() - startDate.getTime());
+    const numberOfDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    const pricePerDay = total_price * numberOfDays;
+    return pricePerDay;
+}
--- backend/routes/booking.routes.js
+++ backend/routes/booking.routes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const { verifyToken } = require('../middleware/auth')
-const { createBooking, getAllBookings, getBooking, getUserBookings,  } = require('../controllers/booking.controller')
+const { createBooking, getAllBookings, getBooking, getUserBookings, editBooking,  } = require('../controllers/booking.controller')
 const router = express.Router()
 
 router.get('/', verifyToken, getAllBookings)
 router.post('/create', verifyToken, createBooking)
 router.get('/user', verifyToken, getUserBookings)
 router.get('/:id', verifyToken, getBooking)
-router.put('/:id')
+router.put('/:id', verifyToken, editBooking)
 router.delete('/:id')
 
 
