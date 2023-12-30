611c3879ec4586bbe9f074501d11d226ebab30cc
mendy <antoine.mendy@hotmail.fr>
2023-12-30 19:10:43 +0100

    feature(front)account menu added

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,14 +1,15 @@
 import { Box, TextField, Typography } from '@mui/material'
 import React from 'react'
+import AccountMenu from './Menu'
 
 const Header = () => {
     return (
         <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', mx: 3,  backgroundColor: '#FFFFEC'}}>
             <Typography>Logo</Typography>
-            <TextField/>
-            <Typography>
-                menu
-            </Typography>
+            <Box sx={{display: 'flex', justifyContent: 'space-around'}}>
+                <TextField/>
+                <AccountMenu/>
+            </Box>
         </Box>
     )
 }
---
+++ front/src/components/Menu.tsx
@@ -0,0 +1,120 @@
+import { Logout, Settings, Checklist, Article, Login } from '@mui/icons-material';
+import { Avatar, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip } from '@mui/material'
+import React, { useState } from 'react'
+import { useAuth } from '../auth/AuthContext';
+
+const AccountMenu = () => {
+    const { isAuthentify } = useAuth();
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    console.log(isAuthentify);
+    
+
+    return (
+        <>
+            <Tooltip title="AccountMenu">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: 2 }}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                >
+                    <Avatar sx={{ width: 32, height: 32, backgroundColor: isAuthentify ? "green" : "red" }}>M</Avatar>
+                </IconButton>
+            </Tooltip>
+            <Menu 
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                slotProps={{  
+                    paper: {
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            width: 250,
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&::before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        }
+
+                    }
+                }} 
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <Avatar />Mon compte
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Article fontSize="small"/>
+                    </ListItemIcon>
+                        Mes articles
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <Checklist /> 
+                    </ListItemIcon>
+
+                    Mes réservations
+                    </MenuItem>
+                    
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Settings
+                    </MenuItem>
+                    {isAuthentify ? (
+                        <MenuItem onClick={handleClose}>
+                            <ListItemIcon>
+                                <Logout fontSize="small" />
+                            </ListItemIcon>
+                                Se déconnecter
+                        </MenuItem>
+                    ) : (
+                        <MenuItem onClick={handleClose}>
+                            <ListItemIcon>
+                                <Logout fontSize="small" />
+                            </ListItemIcon>
+                                Se connecter
+                        </MenuItem> 
+                    )
+                }
+
+            </Menu>
+        
+        </>
+    )
+}
+
+export default AccountMenu
