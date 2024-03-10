6c3b515f3ed57504728c53fe751033b6c74371c2
mendy <antoine.mendy@hotmail.fr>
2024-03-10 22:36:57 +0100

    feature(back):refacto login

--- front/src/auth/login/Login.tsx
+++ front/src/auth/login/Login.tsx
@@ -1,24 +1,43 @@
 import React, { useState } from 'react'
 import Form from '../../components/Form';
+import axios from 'axios';
+import { useAuth } from '../../context/authContext';
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
     const [email, setEmail] = useState<string>('')
     const [password, setPassword] = useState<string>('')
-    const [error, setError] = useState<string | null>();
-    // const {setIsAuthentify} = useAuth()
-    // const navigate = useNavigate()
+    const [error, setError] = useState<string | null>(null);
+    const [userName, setUserName] = useState<string>('');
+    const { setIsAuthentify } = useAuth();
+    const navigate = useNavigate()
 
     const handleSubmit =  async () => {
         if (!email || !password) {
             setError('Tous les champs sont obligatoires');
             return;
         }
+        try {
+            axios.post('http://localhost:4000/user/login', { email, password })
+            .then(res => {
+                const token = res.data.user.token
+                localStorage.setItem('token', token)
+                setIsAuthentify(res.data?.auth);
+                setIsAuthentify(res.data.auth);
+                navigate('/')
+            })
+        } catch (error) {
+            console.error(error);
+            setError('Erreur lors de la connexion. Veuillez réessayer.');
+        };
+        setEmail('')
+        setPassword('')
     }
 
     return (
         <>
-            <Form email={email} setEmail={setEmail} password={password} setPassword={setPassword} error={error} handleSubmit={handleSubmit} setUserName={undefined}/>
+            <Form email={email} setEmail={setEmail} password={password} setPassword={setPassword} error={error} handleSubmit={handleSubmit} setUserName={setUserName} username={userName}/>
         </>
     )
 }
