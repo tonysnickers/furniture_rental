507543b070e74acf9aaf5cfa61114c6283eac2c2
mendy <antoine.mendy@hotmail.fr>
2024-03-19 15:29:21 +0100

    feature(front):navbar redized

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,17 +1,34 @@
-import { Box, TextField, Typography } from '@mui/material'
+import { AppBar, Avatar, Box } from '@mui/material'
 import React from 'react'
 import { IconMenu } from './IconMenu'
+import { SearchBar } from './SearchBar'
 
-const Header = () => {
+export const Header = () => {
     return (
-        <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', px: 3,  backgroundColor: '#0F00'}}>
-            <Typography>Logo</Typography>
+        <AppBar 
+            position="fixed"
+            sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                height: 64,
+                px: 3,
+                py: 5,
+                backgroundColor: 'primary.contrastText',
+                borderBottom: 1,
+                borderBottomColor: 'action.disabledBackground',
+                boxShadow: 'none',
+                width: '100%',
+                zIndex: theme => theme.zIndex.drawer + 1,
+            }}
+        >
+            <Avatar alt="Logo" src={'https://res.cloudinary.com/do9ctd3bd/image/upload/v1710856245/house-3_acwtyn.png'} sx={{ width: 50, height: 50, marginRight: 2 }} />
+            <SearchBar/>
             <Box sx={{display: 'flex', justifyContent: 'space-around'}}>
-                <TextField/>
-                <IconMenu/>
+                <IconMenu/> 
             </Box>
-        </Box>
+        </AppBar>
     )
 }
 
-export default Header
