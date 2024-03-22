bfa9224fb3bf05db833d963b320e74221bc2486b
mendy <antoine.mendy@hotmail.fr>
2024-03-22 23:30:56 +0100

    feature(back)get data from his id

--- front/src/components/Detail.tsx
+++ front/src/components/Detail.tsx
@@ -1,6 +1,29 @@
 import { Box } from '@mui/material'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export const Detail = () => {
+  // const [furniture, setFurniture] = useState()
+  const params = useParams()
+  console.log(params.id);
+  
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const res = await axios.get(`http://localhost:4000/furniture/${params.id}`).then((res) => res.data)
+        console.log(res);
+        
+      } catch (error) {
+        return error
+      }
+    }
+    fetchdata()
+      
+    
+  }, [params])
+  
   return (
     <Box>
       details
