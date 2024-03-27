ebf567b9fa08b890cc67af4877fb2b6728b1f2c3
mendy <antoine.mendy@hotmail.fr>
2024-03-27 23:59:29 +0100

    feature(front): display image carousel in details page

--- front/src/components/Detail.tsx
+++ front/src/components/Detail.tsx
@@ -1,17 +1,32 @@
 import { Box, Typography,  } from '@mui/material'
 import { useParams } from 'react-router-dom'
 import { useFurniture } from '../hooks/use-furniture'
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";   
+import { Image } from '../models/furniture';
 
 export const Detail = () => {
   const params = useParams<{id: string}>()
   const furnitureId = params.id || ''
   const { furniture } = useFurniture(furnitureId)
+  const images = furniture.images ? furniture.images : []
+
+
+  const newImg: {original: string, thumbnail: string}[]  = images.map((img: Image) => {
+    return {original: img.url, thumbnail: img.url}
+  })
   
   
   return (
     <Box sx={{p: 6}}>
-        <Typography variant='h5' sx={{my: 3}}> Location de {furniture.name} à {furniture.city}</Typography>
-        <Typography variant='subtitle1'>{furniture.description}</Typography>
+      <ImageGallery 
+        items={newImg}
+        autoPlay={false}
+        showPlayButton={false}
+        showIndex={true}
+        />
+      <Typography variant='h5' sx={{my: 3}}> Location de {furniture.name} à {furniture.city}</Typography>
+      <Typography variant='subtitle1'>{furniture.description}</Typography>
     </Box>
   )
 }
